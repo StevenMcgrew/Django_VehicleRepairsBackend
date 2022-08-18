0db9edd002f72571a879b9d01f7a686d04123ab5
StevenMcgrew <stevenmcgrew8224@msn.com>
2022-08-18 11:13:22 -0700

    Fixed BlogPost foreign key to User, which was causing relationship error on serialization

--- project/vehicle_repairs/serializers.py
+++ project/vehicle_repairs/serializers.py
@@ -3,14 +3,6 @@ from django.contrib.auth.models import User
 from vehicle_repairs.models import BlogPost
 from vehicle_repairs.models import Vehicle
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    blog_posts = serializers.HyperlinkedRelatedField(many=True,
-                    view_name='blogpost-detail', read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['url', 'id', 'username', 'blog_posts']
-
 
 class BlogPostSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
@@ -18,7 +10,16 @@ class BlogPostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = BlogPost
         fields = ['url', 'id', 'title', 'repair_steps', 'thumbnail',
-                  'is_published', 'like_count', 'user', 'vehicle']
+                  'is_published', 'user', 'vehicle']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    blogposts = serializers.HyperlinkedRelatedField(many=True,
+                                                     view_name='blogpost-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'blogposts']
 
 
 class VehicleSerializer(serializers.HyperlinkedModelSerializer):
